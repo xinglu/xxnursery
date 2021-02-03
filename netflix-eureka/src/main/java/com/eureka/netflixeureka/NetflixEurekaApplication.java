27dcc239a2383f466de3f4f8cc30f0638bd48071
梅士强 <13243038531@163.com>
2021-02-03 17:30:03 +0800

    添加注册中心eureka服务

---
+++ netflix-eureka/src/main/java/com/eureka/netflixeureka/NetflixEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.netflixeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//
+public class NetflixEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaApplication.class, args);
+    }
+
+}
---
+++ netflix-eureka/src/test/java/com/eureka/netflixeureka/NetflixEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.netflixeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
